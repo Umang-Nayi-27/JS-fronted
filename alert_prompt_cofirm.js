4fd51df2c84b13e0f16a3012b188bfadfc59a500
Umang-Nayi-27 <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-09-03 18:08:49 +0530

    Create alert_prompt_cofirm.js

---
+++ alert_prompt_cofirm.js
@@ -0,0 +1,13 @@
+//alert :::
+  //--> aa jya sudhi accept nai thay tya sudhi webpage stop thai jayyy... code work thai jase only ...webpage stop thai jay...
+  // alert("stops the webpage")
+
+//prompt ::::
+  //user pase thi alert vade input leva mate vapray che....
+  // let x = prompt("ask question","default answer of prmopt");
+
+//confirm :::
+  // warning athva confirm kerva matee....
+  // true false ma response aave...-->> ok click keris to "true", cancle keris to "false"
+  //let a = confirm("   msg   ");
+  //console.log(a);
